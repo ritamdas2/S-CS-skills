0fc9644b41ccb08138633e2db0718158cf0dc91d
Ritam Das <37518854+ritamdas2@users.noreply.github.com>
2020-10-07 17:40:10 -0400

    Add files via upload

---
+++ skills/cluster-2/16/code/serial.js
@@ -0,0 +1,11 @@
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('/dev/cu.SLAB_USBtoUART', { baudRate: 115200 });
+const parse = port.pipe(new Readline({ delimiter: '\n' }));
+// Read the port data
+port.on("open", () => {
+  console.log('serial port open');
+});
+parse.on('data', data =>{
+  console.log('output from esp:', data);
+});
