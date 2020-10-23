d3513ddac11bfd08220b8ba4793559e3a4865ddb
Ritam Das <37518854+ritamdas2@users.noreply.github.com>
2020-10-23 19:07:21 -0400

    Create led_example_main_auto.c

---
+++ skills/cluster-3/24/code/led_example_main_auto.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "driver/uart.h"
+#include "esp_vfs_dev.h"
+
+/*
+ * About this example
+ *
+ * 1. Start with initializing LEDC module:
+ *    a. Set the timer of LEDC first, this determines the frequency
+ *       and resolution of PWM.
+ *    b. Then set the LEDC channel you want to use,
+ *       and bind with one of the timers.
+ *
+ * 2. You need first to install a default fade function,
+ *    then you can use fade APIs.
+ *
+ * 3. You can also set a target duty directly without fading.
+ *
+ * 4. This example uses GPIO18/19/4/5 as LEDC output,
+ *    and it will change the duty repeatedly.
+ *
+ * 5. GPIO18/19 are from high speed channel group.
+ *    GPIO4/5 are from low speed channel group.
+ *
+ */
+#define LEDC_HS_TIMER          LEDC_TIMER_0
+#define LEDC_HS_MODE           LEDC_HIGH_SPEED_MODE
+#define LEDC_HS_CH0_GPIO       (12)
+#define LEDC_HS_CH0_CHANNEL    LEDC_CHANNEL_0
+
+#define LEDC_LS_TIMER          LEDC_TIMER_1
+#define LEDC_LS_MODE           LEDC_LOW_SPEED_MODE
+
+#define LEDC_TEST_CH_NUM       (4)
+#define LEDC_TEST_DUTY         (4000)
+#define LEDC_TEST_FADE_TIME    (3000)
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK( uart_driver_install(UART_NUM_0,
+      256, 0, 0, NULL, 0) );
+
+/* Tell VFS to use UART driver */
+  esp_vfs_dev_uart_use_driver(UART_NUM_0);
+    int ch;
+
+    /*
+     * Prepare and set configuration of timers
+     * that will be used by LED Controller
+     */
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
+        .freq_hz = 5000,                      // frequency of PWM signal
+        .speed_mode = LEDC_HS_MODE,           // timer mode
+        .timer_num = LEDC_HS_TIMER,            // timer index
+        .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
+    };
+    // Set configuration of timer0 for high speed channels
+    ledc_timer_config(&ledc_timer);
+
+    // Prepare and set configuration of timer1 for low speed channels
+    ledc_timer.speed_mode = LEDC_LS_MODE;
+    ledc_timer.timer_num = LEDC_LS_TIMER;
+    ledc_timer_config(&ledc_timer);
+
+    /*
+     * Prepare individual configuration
+     * for each channel of LED Controller
+     * by selecting:
+     * - controller's channel number
+     * - output duty cycle, set initially to 0
+     * - GPIO number where LED is connected to
+     * - speed mode, either high or low
+     * - timer servicing selected channel
+     *   Note: if different channels use one timer,
+     *         then frequency and bit_num of these channels
+     *         will be the same
+     */
+    ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
+        {
+            .channel    = LEDC_HS_CH0_CHANNEL,
+            .duty       = 0,
+            .gpio_num   = LEDC_HS_CH0_GPIO,
+            .speed_mode = LEDC_HS_MODE,
+            .hpoint     = 0,
+            .timer_sel  = LEDC_HS_TIMER
+        }
+    };
+
+    // Set LED Controller with previously prepared configuration
+    for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
+        ledc_channel_config(&ledc_channel[ch]);
+    }
+
+    // Initialize fade service.
+    ledc_fade_func_install(0);
+    while (1) {
+   
+        for (int i = 0; i < 10; i++) {
+            ledc_set_duty(ledc_channel[0].speed_mode, ledc_channel[0].channel, LEDC_TEST_DUTY*i*0.1);
+            ledc_update_duty(ledc_channel[0].speed_mode, ledc_channel[0].channel);
+            vTaskDelay(250 / portTICK_PERIOD_MS);
+        }
+        for (int j = 9; j > -1; j--) {
+            ledc_set_duty(ledc_channel[0].speed_mode, ledc_channel[0].channel, LEDC_TEST_DUTY*j*0.1);
+            ledc_update_duty(ledc_channel[0].speed_mode, ledc_channel[0].channel);
+            vTaskDelay(250 / portTICK_PERIOD_MS);
+        }
+
+
+    }
+}
